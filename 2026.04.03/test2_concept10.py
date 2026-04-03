c4b7b97dcf94d70ddbf65b7a3b5712c870ffc396
DoingCrong <sk690460@gmail.com>
2026-04-03 13:54:04 +0900

    크롤링2_self
    1)test_concept -> test2_concept.html로 변경
    - 이름이 겹칠듯하여 변경함
    
    2) 만약 크롤링을 사용하여 정보를 뽑아와야하는 상황이 있을꺼같으면
    초기에 class나 id값 세팅을 잘해야 할듯

---
+++ 2026.04.03/test2_concept10.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+
+# 파일 읽기
+with open("C:/Users/1Class_015/Desktop/LEEHONGJUNE/Python/Git_Self_Python/2026.04.03/test2_concept10.html", "r", encoding="utf-8") as file:
+    html = file.read()
+
+# 파싱
+soup = BeautifulSoup(html, "html.parser")
+
+# 전체 상품 가져오기
+items = soup.find_all("table", class_="join")
+
+# 결과 출력
+for item in items:
+    test1 = item.find("option", id="select").text
+    test2 = item.find("input", id="name").text
+    test3 = item.find("input", id="password").text
+    test4 = item.find("option", id="codding").text
+    test5 = item.find("option", id="music").text
+
+    if not (test2):
+        test2 = "비어있습니다."
+
+    if test3 is None: #null체크용도 : null도값이다
+        test3 = "비어있습니다."
+    else:
+        test3 = "null도 값이다."
+
+    print(f"문구1: {test1}\n문구2: {test2}\n문구3: {test3}\n문구4: {test4}\n문구5: {test5}")
