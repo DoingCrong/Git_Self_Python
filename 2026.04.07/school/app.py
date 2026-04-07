6fb70db26f3cd2b14615d46711e9fe18bb68986a
DoingCrong <sk690460@gmail.com>
2026-04-07 14:13:50 +0900

    DB연결 [select]Part
    1) app.py
    
        if name=="" or kor==""or eng==""or math=="":
            return "모든값을 입력하세요. <a href="/">돌아가기</a>"
        #값이 비어있으면 돌아가는 구문
        #굳이 작성하기 싫으면 html에서 input구문에 required작성
    
        #Mysql selectAll
        #기존에는 자료가 적어서 일일이 던지면 됬는데 자료가 100개면 100개를 던질수 없으니까
        #하나의 변수에 담아서 던진 후 result.html로 for문을 돌려 출력하려는 내용
        cursor = conn.cursor(pymysql.cursors.DictCursor) #데이터 베이스에 들어있는 값을
                                                        #딕셔너리로 변경하여 저장
        sql = "select * from student"
        cursor.execute(sql)
        rows = cursor.fetchall() #하나면 row = cursor.fetchone()
    
    2) result.html
    html에서 python명령어를 사용하려면 : {% for row in rows %}
    html에서 python변수를 사용하려면 : {{name}}
    html에서 python for문을 사용하려면2 : 👍
    {% for row in rows %}
    {{row.id}}
    ...
    {% endfor %}
    
    - table에서
            <!--python에서 table for문 작성하는법 -->
            {% for row in rows %}
            <tr>
                <td>{{row.id}}</td>
                <td>{{row.name}}</td>
                <td>{{row.kor}}</td>
                <td>{{row.eng}}</td>
                <td>{{row.math}}</td>
                <td>{{row.total}}</td>
                <td>{{row.avg}}</td>
                <td>{{row.grade}} </td>
            </tr>
            {% endfor %}

--- 2026.04.07/school/app.py
+++ 2026.04.07/school/app.py
@@ -29,10 +29,15 @@ def insert():
     cursor = conn.cursor()
 
     #index.html에서 받아온 값
-    name = request.form['name']
-    kor = int(request.form['kor'])
-    eng = int(request.form['eng'])
-    math = int(request.form['math'])
+    name = request.form['name'].strip()
+    kor = int(request.form['kor'].strip())
+    eng = int(request.form['eng'].strip())
+    math = int(request.form['math'].strip())
+
+    if name=="" or kor==""or eng==""or math=="":
+        return "모든값을 입력하세요. <a href="/">돌아가기</a>" 
+    #값이 비어있으면 돌아가는 구문
+    #굳이 작성하기 싫으면 html에서 input구문에 required작성
 
     total = kor+eng+math
     avg = float(total/3)
@@ -48,7 +53,7 @@ def insert():
     else:
         grade = 'F'
 
-    #Mysql DB에 값대입
+    #Mysql DB에 값대입(insert)
     sql = "insert into student(name, kor, eng, math, total, avg, grade)" \
           "values(%s, %s, %s, %s, %s, %s, %s)"
     
@@ -57,18 +62,30 @@ def insert():
     cursor.execute(sql,(name, kor, eng, math, total, avg, grade))
     conn.commit()
 
+    #Mysql selectAll
+    #기존에는 자료가 적어서 일일이 던지면 됬는데 자료가 100개면 100개를 던질수 없으니까
+    #하나의 변수에 담아서 던진 후 result.html로 for문을 돌려 출력하려는 내용
+    cursor = conn.cursor(pymysql.cursors.DictCursor) #데이터 베이스에 들어있는 값을
+                                                    #딕셔너리로 변경하여 저장
+    sql = "select * from student"
+    cursor.execute(sql)
+    rows = cursor.fetchall() #하나면 row = cursor.fetchone()
+
+
     #.close
     cursor.close()
     conn.close()
 
-    return render_template('result.html',
-                        name=name,
-                        kor=kor,
-                        eng=eng,
-                        math=math,
-                        total=total,
-                        avg=avg,
-                        grade=grade)
+    return render_template('result.html', rows=rows
+                        #기존방식
+                        # name=name, 
+                        # kor=kor,
+                        # eng=eng,
+                        # math=math,
+                        # total=total,
+                        # avg=avg,
+                        # grade=grade
+                        )
 
 if __name__ == '__main__':
     app.run(debug=True)
