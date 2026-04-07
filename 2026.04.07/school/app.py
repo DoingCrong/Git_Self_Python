aca53bf6ac36853d24530fd2c8b10f18f6c1bd01
DoingCrong <sk690460@gmail.com>
2026-04-07 12:26:44 +0900

    DB연결 [insert]Part
    1)
    import pymysql
    
    #db연동함수 생성
    def get_connection():
        return pymysql.connect(
            host='localhost',
            user='root',
            password='1234',
            database="flaskdb",
            charset="utf8"
        )
    
    #insert
        #db 연결 변수 생성
        conn = get_connection()
        cursor = conn.cursor()
    
        #cursor로 execute(insert, update, delete에서만)
        #conn으로 commit
        cursor.execute(sql,(name, kor, eng, math, total, avg, grade))
        conn.commit()
    
        #.close
        cursor.close()
        conn.close()

--- 2026.04.07/school/app.py
+++ 2026.04.07/school/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+import pymysql
+
+#flask 객체 생성
 
 app = Flask(__name__)
+
+#db연동함수 생성
+def get_connection():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='1234',
+        database="flaskdb",
+        charset="utf8"
+    )
 
 #home
 @app.route('/')
@@ -11,6 +24,11 @@ def index():
 @app.route('/insert', methods=['POST'])
 def insert():
 
+    #db 연결 변수 생성
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    #index.html에서 받아온 값
     name = request.form['name']
     kor = int(request.form['kor'])
     eng = int(request.form['eng'])
@@ -29,6 +47,19 @@ def insert():
         grade = 'D'
     else:
         grade = 'F'
+
+    #Mysql DB에 값대입
+    sql = "insert into student(name, kor, eng, math, total, avg, grade)" \
+          "values(%s, %s, %s, %s, %s, %s, %s)"
+    
+    #cursor로 execute(insert, update, delete에서만)
+    #conn으로 commit
+    cursor.execute(sql,(name, kor, eng, math, total, avg, grade))
+    conn.commit()
+
+    #.close
+    cursor.close()
+    conn.close()
 
     return render_template('result.html',
                         name=name,
